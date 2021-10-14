4ad5c0743897174887f651b6d6ae11698289e9f3
wyang <william.yang@uq.edu.au>
2021-10-14 21:44:44 +1000

    changed to list of strings

--- flaskexample2.py
+++ flaskexample2.py
@@ -13,19 +13,17 @@ app = Flask(__name__)
 
 articles_org = pd.read_csv('https://raw.githubusercontent.com/wyang23/architectgooddata/main/gooddata.csv', encoding = 'ISO-8859-1')
 
-#articles_org = pd.read_csv(url)
-print(articles_org.head(3))
+
 articles = articles_org[[ 'Author', 'Title','Keywords']]
 articles['combined_features'] = articles['Author'] +' '+ articles['Title'] +' '+ articles['Keywords']
 
 articles.iloc[0]['combined_features']
 
-print(articles)
 cv = CountVectorizer()
 count_matrix = cv.fit_transform(articles['combined_features'])
 cs = cosine_similarity(count_matrix)
 cs.shape
-#print(articles_org[1][1])
+
 def get_article_title_from_index(index):
     return articles_org[articles_org['Index']==index]['Title'].values[0]
 def get_index_from_article_title(name):
@@ -33,24 +31,19 @@ def get_index_from_article_title(name):
 
 @app.route('/api', methods = ['GET'])
 def getRecommendations():
-
+    articleString = ""
     inputchr = str(request.args['query'])
-    print(request.args['query'])
-    print(inputchr)
     #test_article_title = input('Enter Article name --> ')
     test_article_title = inputchr
     test_article_index = get_index_from_article_title(test_article_title)
     articles_corrs = cs[test_article_index]
     articles_corrs = enumerate(articles_corrs)
     sorted_similar_articles = sorted(articles_corrs,key=lambda x:x[1],reverse=True)
-    print(sorted_similar_articles[1][0])
-    return get_article_title_from_index(sorted_similar_articles[1][0])
-    
-
-#getRecommendations(title="The challenge of opening up gated communities in Shanghai")
-#title = "yes"
+    #return get_article_title_from_index(sorted_similar_articles[1][0])
+    for i in range(4):
+            articleString.append(get_article_title_from_index(sorted_similar_articles[i+1][0]))
+            articleString.append(":")
+    return articleString[:-1]
 
 if __name__ == '__main__':
-    #getRecommendations(title="The challenge of opening up gated communities in Shanghai")
     app.run(host='0.0.0.0')
-    #app.run(host='0.0.0.0', port='8080', debug=True)
